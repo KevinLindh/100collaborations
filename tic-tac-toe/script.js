d3d29116dc00d86d52b4252bc17c077a3aa234fd
Kevin Lindholm <kevin.a.lindholm@hotmail.com>
2022-05-04 22:53:30 +0300

    add basic structure of ´tictactoe class

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,24 @@
+class TicTacToe {
+    constructor(board, symbolPlayer1, symbolPLayer2, player1Turn){
+        this.board = board
+        this.symbolPlayer1 = symbolPlayer1
+        this.symbolPLayer2 = symbolPLayer2
+        this.player1Turn = true
+        this.GameOver = false
+        this.count = 0
+    }
+    //change the value in Dom depending on where player clicked and what value they have "x" or "o"
+    display(){
+
+    }
+    //Method to check if either player has won and seperate check for tie
+    checkForWin(){
+
+    }
+    //Method that resets the board to the beginning
+    reset(){
+
+    }
+}
+
+//create the game object from TicTacToe
